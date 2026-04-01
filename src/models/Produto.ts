275dfc449eed6e701eb5f910f5b16e47a44efa28
Silas Pereira Martins <pet0033648@aluno.firjansenaisesi.com.br>
2026-04-01 19:46:37 -0300

    feat: implementa CRUD completo no model de produtos

--- src/models/Produto.ts
+++ src/models/Produto.ts
@@ -18,5 +18,22 @@ export const ProdutoModel = {
         const values = [dados.nome, dados.preco, dados.estoque];
         const result = await pool.query(query, values);
         return result.rows[0]
+    },
+
+    async buscarPorID(id:Pick<IProduto, "id">):Promise<IProduto | null> {
+        const query = "SELECT * FROM produtos WHERE id = $1"
+        const { rows } = await pool.query(query,[id]);
+        return rows [0] || null;
+    },
+    async atualizar(id:Pick<IProduto, "id">, dados: Partial<IProduto>): Promise<IProduto | null> {
+        const query = "UPDATE produtos SET nome = $1, preco = $2, estoque = $3 WHERE id = $4 RETURNING *"
+        const values = [dados.nome, dados.preco, dados.estoque];
+        const result = await pool.query(query, values);
+        return result.rows [0] || null;
+    },
+
+    async delete(id:Pick<IProduto, "id">):Promise<boolean> {
+        const result = await pool.query("DELETE FROM produtos WHERE ID = $1", [id]);
+        return (result.rowCount ?? 0) > 0
     }
 }
